ac819806e4cb2b271de4f8fefbb5f65db5714a39
一纸忘忧 <i@ikxin.com>
2024-04-20 23:48:30 +0800

    feat: update the remaining field Chinese translation

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -6,7 +6,7 @@ const description =
     '适合人体工程学的框架，由 Bun 提供支持的 TypeScript 框架，具有端到端类型安全、统一的类型系统和出色的开发人员体验。'
 
 export default defineConfig({
-    lang: 'zh-CN',
+    lang: 'zh-Hans',
     title: 'ElysiaJS 中文文档',
     // description,
     ignoreDeadLinks: true,
@@ -109,7 +109,28 @@ export default defineConfig({
     ],
     themeConfig: {
         search: {
-            provider: 'local'
+            provider: 'local',
+            options: {
+                locales: {
+                    root: {
+                        translations: {
+                            button: {
+                                buttonText: '搜索文档',
+                                buttonAriaLabel: '搜索文档'
+                            },
+                            modal: {
+                                noResultsText: '无法找到相关结果',
+                                resetButtonTitle: '清除查询条件',
+                                footer: {
+                                    selectText: '选择',
+                                    navigateText: '切换',
+                                    closeText: '关闭',
+                                }
+                            },
+                        }
+                    }
+                }
+            }
         },
         logo: '/assets/elysia.svg',
         nav: [
@@ -460,6 +481,27 @@ export default defineConfig({
             text: '在 GitHub 上编辑此页面',
             pattern:
                 'https://github.com/ikxin/elysia-docs-cn/edit/main/docs/:path'
-        }
-    }
+        },
+        docFooter: {
+            prev: '上一页',
+            next: '下一页'
+        },
+        outline: {
+            label: '页面导航'
+        },
+
+        lastUpdated: {
+            text: '最后更新于',
+            formatOptions: {
+                dateStyle: 'short',
+                timeStyle: 'medium'
+            }
+        },
+        langMenuLabel: '多语言',
+        returnToTopLabel: '回到顶部',
+        sidebarMenuLabel: '菜单',
+        darkModeSwitchLabel: '主题',
+        lightModeSwitchTitle: '切换到浅色模式',
+        darkModeSwitchTitle: '切换到深色模式'
+    },
 })
